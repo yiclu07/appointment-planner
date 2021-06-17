41adda21a22fea2baaeef87aab34af3e67982f1a
Red Roc <yi.c.lu07@gmail.com>
2021-06-16 22:17:58 -0400

    Add and copy initial files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route, Redirect, NavLink } from 'react-router-dom';
+
+import { AppointmentsPage } from './containers/appointmentsPage/AppointmentsPage';
+import { ContactsPage } from './containers/contactsPage/ContactsPage';
 
 function App() {
+  /* 
+  Define state variables for 
+  contacts and appointments
+  */
+
+  const ROUTES = {
+    CONTACTS: '/contacts',
+    APPOINTMENTS: '/appointments',
+  };
+
+  /* 
+  Implement functions to add data to
+  contacts and appointments
+  */
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <NavLink to={ROUTES.CONTACTS} activeClassName='active'>
+          Contacts
+        </NavLink>
+        <NavLink to={ROUTES.APPOINTMENTS} activeClassName='active'>
+          Appointsments
+        </NavLink>
+      </nav>
+      <main>
+        <Switch>
+          <Route path={ROUTES.CONTACTS}>
+            {/* Add props to ContactsPage */}
+            <ContactsPage />
+          </Route>
+          <Route path={ROUTES.APPOINTMENTS}>
+            {/* Add props to AppointmentsPage */}
+            <AppointmentsPage />
+          </Route>
+        </Switch>
+      </main>
+    </>
   );
 }
 
