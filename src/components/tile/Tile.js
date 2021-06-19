c223dee0e75feb221ba5bb98c3b7186155769a9c
Red Roc <yi.c.lu07@gmail.com>
2021-06-19 00:55:43 -0400

    Add functionality to render contacts and appointments

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-export const Tile = () => {
+export const Tile = ({ item }) => {
+  const titles = Object.values(item);
+
   return (
     <div className='tile-container'>
-
+      {titles.map((title, index) => {
+        return <p className={index === 0 ? "tile-title" : "tile"} key={index}>{title}</p>
+      })}
     </div>
   );
 };
