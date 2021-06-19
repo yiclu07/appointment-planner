c4fd5b4436d200e2ffae19e6ed4548e07ef0999a
Red Roc <yi.c.lu07@gmail.com>
2021-06-19 00:28:31 -0400

    Add functionality to render prop as a list

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Tile } from '../tile/Tile.js';
 
-export const TileList = () => {
+export const TileList = ({ list }) => {
   return (
     <div>
-
+      {list.map((item) => {
+        return <Tile item={item} />
+      })}
     </div>
   );
 };
