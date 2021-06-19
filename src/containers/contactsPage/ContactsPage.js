ab790df4df7258653e13b5b612a829002e0c138e
Red Roc <yi.c.lu07@gmail.com>
2021-06-19 00:29:37 -0400

    Change prop name to better reflect usage

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -53,7 +53,7 @@ export const ContactsPage = (props) => {
       <hr />
       <section>
         <h2>Contacts</h2>
-        <TileList contacts={contacts} />
+        <TileList list={contacts} />
       </section>
     </div>
   );
