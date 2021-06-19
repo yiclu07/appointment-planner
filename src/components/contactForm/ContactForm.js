48e2ea1315a35951837012f51aa4882fdc015507
Red Roc <yi.c.lu07@gmail.com>
2021-06-18 22:35:55 -0400

    Add phone regex

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -14,9 +14,9 @@ export const ContactForm = ({
       <input type="text" value={newName} placeholder="Name" onChange={(e) => {
         setNewName(e.target.value);
       }} />
-      <input type="tel" value={newPhone} placeholder="Phone" onChange={(e) => {
+      <input type="tel" value={newPhone} placeholder="Phone (123-456-7890)" onChange={(e) => {
         setNewPhone(e.target.value);
-      }} />
+      }} pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}" />
       <input type="email" value={newEmail} placeholder="Email" onChange={(e) => {
         setNewEmail(e.target.value);
       }} />
