0a7f4f2e708616e9781d9422270de6cad9a43c8e
Red Roc <yi.c.lu07@gmail.com>
2021-06-20 01:08:55 -0400

    Render contact options for appointments

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-export const ContactPicker = () => {
+export const ContactPicker = ({ contacts }) => {
   return (
-    ContactPicker
+    <select>
+      <option value="">---Please choose an option---</option>
+      {contacts.map((contact, index) => {
+        return <option key={index} value={contact.name}>{contact.name}</option>
+      })}
+    </select>
   );
 };
